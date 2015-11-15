52edfa5cf63ae087af09db6c996aed0af1d9a66e
caixukun <2468085704@qq.com>
2015-11-15 16:20:35 +0800

    a

---
+++ Two/TestSuite/Test.cpp
@@ -0,0 +1,33 @@
+#include "Test.h"
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+using namespace TestSuite;
+
+void Test::do_test(bool cond, const std::string& lbl, const char* fname, long libneno){
+	if(!cond){
+		do_fail(lbl, fname, libneno);
+	}else{
+		succeed_();
+	}
+}
+
+void Test::do_fail(const std::string& lbl, const char* fname, long lineno){
+	++nFail;
+	if(osptr){
+		*osptr << typeid(*this).name()
+			   << "failure: (" << lbl << ") , " << fname
+			   << " (line" << lineno <<")" << endl;
+	}
+}
+
+long Test::report() const{
+	if(osptr){
+		*osptr << "Test \"" << typeid(*this).name()
+			   << "\":\n\tPassed: " << nPass
+			   << "\tFailed: " << nFail
+			   << endl;
+	}
+	return nFail;
+}
