4e4cea8e2b127cce20272fffe1dcb5caf605486f
caixukun <2468085704@qq.com>
2015-11-20 13:22:33 +0800

    d

--- Two/C05/Box1.cpp
+++ Two/C05/Box1.cpp
@@ -5,7 +5,7 @@ template<class T>
 class Box;
 
 template<class T>
-Box<T> operator+(const Box<T>&, cosnt Box<T>&);
+Box<T> operator+(const Box<T>&, const Box<T>&);
 
 template<class T>
 ostream& operator<<(ostream&, const Box<T>&);
@@ -14,5 +14,22 @@ template<class T>
 class Box{
     T t;
 public:
-    Box()
+    Box(const T& theT) : t(theT){}
+	friend Box operator+<>(const Box<T>&, const Box<T>&);
+	friend ostream& operator<< <>(ostream&, const Box<T>&);
 };
+
+template<class T>
+Box<T> operator+(const Box<T>& b1, const Box<T>& b2){
+	return Box<T>(b1.t + b2.t);
+}
+
+template<class T>
+ostream& operator<<(ostream& os, const Box<T>& b){
+	return os << '[' << b.t << ']';
+}
+
+int main(){
+	Box<int> b1(1), b2(2);
+	cout << b1 + b2 << endl;
+}
