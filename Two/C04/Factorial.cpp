a7503ebdb3644ead7becca407529847db80c46aa
caixukun <2468085704@qq.com>
2015-11-21 09:00:34 +0800

    d

---
+++ Two/C04/Factorial.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template<int n>
+struct Factorial{
+	enum{val=Factorial<n-1>::val * n};
+};
+
+template<>
+struct Factorial<0>{
+	enum{val=1};
+};
+
+int main(){
+	cout << Factorial<12>::val << endl;
+}
