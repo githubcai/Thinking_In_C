142542352778f0341e9e5aa7475e7a6545cbd8af
caixukun <2468085704@qq.com>
2015-11-21 10:39:30 +0800

    d

---
+++ Two/C05/Fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+template<int n>
+struct Fib{
+    enum{val=Fib<n-1>::val + Fib<n-2>::val};
+};
+
+template<>
+struct  Fib<1>{
+    enum{val=1};
+};
+
+template<>
+struct Fib<0>{
+    enum{val=0};
+};
+
+int main(){
+    cout << Fib<5>::val << endl;
+    cout << Fib<20>::val << endl;
+}
